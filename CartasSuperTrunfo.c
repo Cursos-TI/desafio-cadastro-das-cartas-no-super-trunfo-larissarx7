1e9908e21824c13d50588267d4cbb09ca672dba2
Larissa Nascimento <196659475+larissarx7@users.noreply.github.com>
2025-02-26 10:53:41 -0300

    CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,121 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
-
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    // Definição das variáveis para a primeira carta
+    char estado1;
+    int cidade_num1;
+    int populacao1, area1, pib1, pontos_turisticos1;
+    char nome1[4];
+    float densidade_populacional1, pib_per_capita1, super_poder1;
+
+    // Definição das variáveis para a segunda carta
+    char estado2;
+    int cidade_num2;
+    int populacao2, area2, pib2, pontos_turisticos2;
+    char nome2[4];
+    float densidade_populacional2, pib_per_capita2, super_poder2;
+
+    // Entrada de dados para a primeira carta
+    printf("Cadastro da primeira carta de cidade - Super Trunfo: Países (Nível Mestre)\n");
+    printf("Informe o estado (A-H) da primeira carta: ");
+    scanf(" %c", &estado1);
+
+    printf("Informe o número da cidade (1-4) da primeira carta: ");
+    scanf("%d", &cidade_num1);
+
+    sprintf(nome1, "%c%02d", estado1, cidade_num1);
+
+    printf("Informe a população da cidade: ");
+    scanf("%d", &populacao1);
+
+    printf("Informe a área da cidade (em km²): ");
+    scanf("%d", &area1);
+
+    printf("Informe o PIB da cidade (em bilhões): ");
+    scanf("%d", &pib1);
+
+    printf("Informe o número de pontos turísticos da cidade: ");
+    scanf("%d", &pontos_turisticos1);
+
+    // Cálculos das propriedades para a primeira carta
+    densidade_populacional1 = (float)populacao1 / area1;
+    pib_per_capita1 = (float)pib1 * 1000000000 / populacao1;
+    super_poder1 = populacao1 + area1 + pib1 + pontos_turisticos1 + densidade_populacional1 + pib_per_capita1;
+
+    // Entrada de dados para a segunda carta
+    printf("\nCadastro da segunda carta de cidade - Super Trunfo: Países (Nível Mestre)\n");
+    printf("Informe o estado (A-H) da segunda carta: ");
+    scanf(" %c", &estado2);
+
+    printf("Informe o número da cidade (1-4) da segunda carta: ");
+    scanf("%d", &cidade_num2);
+
+    sprintf(nome2, "%c%02d", estado2, cidade_num2);
+
+    printf("Informe a população da cidade: ");
+    scanf("%d", &populacao2);
+
+    printf("Informe a área da cidade (em km²): ");
+    scanf("%d", &area2);
+
+    printf("Informe o PIB da cidade (em bilhões): ");
+    scanf("%d", &pib2);
+
+    printf("Informe o número de pontos turísticos da cidade: ");
+    scanf("%d", &pontos_turisticos2);
+
+    // Cálculos das propriedades para a segunda carta
+    densidade_populacional2 = (float)populacao2 / area2;
+    pib_per_capita2 = (float)pib2 * 1000000000 / populacao2;
+    super_poder2 = populacao2 + area2 + pib2 + pontos_turisticos2 + densidade_populacional2 + pib_per_capita2;
+
+    // Comparação das cartas
+    printf("\nComparando as cartas...\n");
+
+    // Comparação das propriedades
+    printf("\nPropriedades comparadas:\n");
+
+    // Densidade Populacional (menor vence)
+    if (densidade_populacional1 < densidade_populacional2) {
+        printf("Densidade Populacional: Carta 1 vence\n");
+    } else {
+        printf("Densidade Populacional: Carta 2 vence\n");
+    }
+
+    // População (maior vence)
+    if (populacao1 > populacao2) {
+        printf("População: Carta 1 vence\n");
+    } else {
+        printf("População: Carta 2 vence\n");
+    }
+
+    // Área (maior vence)
+    if (area1 > area2) {
+        printf("Área: Carta 1 vence\n");
+    } else {
+        printf("Área: Carta 2 vence\n");
+    }
+
+    // PIB (maior vence)
+    if (pib1 > pib2) {
+        printf("PIB: Carta 1 vence\n");
+    } else {
+        printf("PIB: Carta 2 vence\n");
+    }
+
+    // Pontos turísticos (maior vence)
+    if (pontos_turisticos1 > pontos_turisticos2) {
+        printf("Pontos turísticos: Carta 1 vence\n");
+    } else {
+        printf("Pontos turísticos: Carta 2 vence\n");
+    }
+
+    // Super Poder (maior vence)
+    if (super_poder1 > super_poder2) {
+        printf("Super Poder: Carta 1 vence\n");
+    } else {
+        printf("Super Poder: Carta 2 vence\n");
+    }
 
     return 0;
 }
